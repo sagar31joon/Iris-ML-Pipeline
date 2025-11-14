9eb62d9fa95cca253bae53127e0f16c56b68ca5c
sagar31joon <99651581+sagar31joon@users.noreply.github.com>
2025-11-15 04:48:45 +0530

    make models

---
+++ preparing_data.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datasets import load_dataset
+from sklearn.model_selection import train_test_split
+
+#iris_dataset = load_dataset("sasageooo/iris-flower-classification") #loading dataset from hugging face
+
+#iris_dataset['train'].to_csv("iris_dataset.csv") #saving dataset as CSV file
+
+df = pd.read_csv("dataset/iris_dataset.csv")
+print(df)
+#print(df.head()) 
+print(df.info()) 
+print(df.describe()) 
+
+#plot1 = sns.pairplot(df, hue = "species") #plotting dataset
+#print(plot1)
+#plt.show()
+
+from sklearn.preprocessing import LabelEncoder #label encoding
+le = LabelEncoder()
+df['species'] = le.fit_transform(df['species']) #encoding species column
+
+dataset_values = df.values #slicing dataset
+x = dataset_values[:, 0:4]
+y = dataset_values[:, 4]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42) #splitting into train & test sets
+print("Train split : ")
+print(x_train, y_train)
+print("Test split : ")
+print(x_test, y_test)
+
+from sklearn.preprocessing import StandardScaler #standardizing data so that all features have mean of 0 and sd of 1
+sc = StandardScaler() 
+x_train = sc.fit_transform(x_train) #fit and transform train data
+x_test = sc.transform(x_test) #only transform test data
+
+print("Train split : ")
+print(x_train, y_train)
+print("Test split : ")
+print(x_test, y_test)
+
+
+
---
+++ readme.txt
@@ -0,0 +1,25 @@
+iris_project/
+│
+├── dataset/
+│   ├── iris_raw.csv
+│   └── iris_processed.csv
+│
+├── models/
+│   ├── logistic_regression.pkl
+│   ├── decision_tree.pkl
+│   ├── svm.pkl
+│   └── scaler.pkl
+│
+├── src/
+│   ├── prepare_data.py
+│   ├── train_LR.py
+│   ├── train_decision_tree.py
+│   ├── train_SVM.py
+│   ├── predict_LR.py
+│   ├── predict_decision_tree.py
+│   ├── predict_SVM.py
+│
+├── samples/
+│
+└── main.py
+
